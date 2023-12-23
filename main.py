6bbcf32b8d34f34106e4443d48e5280e784bacbc
João Pantaleão <jopantaleao@gmail.com>
2023-12-23 16:12:56 +0000

    done

--- main.py
+++ main.py
@@ -15,42 +15,46 @@ quote_data = requests.get(url=f"{URL}/quote", headers=headers)
 quote_data.raise_for_status()
 data = quote_data.json()
 
+def generate_quote():
+    random_number = randint(0, 999)
+    quote_json = data["docs"][random_number]
 
-random_number = randint(0,999)
-quote_json = data["docs"][random_number]
+    # getting quote
+    quote = quote_json["dialog"]
 
-# getting quote
-quote = quote_json["dialog"]
+    # getting character's name
+    character_id = quote_json["character"]
+    character_data = requests.get(url=f"{URL}character/{character_id}", headers=headers)
+    character_data.raise_for_status()
+    character_json = character_data.json()
+    character = character_json['docs'][0]["name"]
 
-# getting character's name
-character_id = quote_json["character"]
-character_data = requests.get(url=f"{URL}character/{character_id}", headers=headers)
-character_data.raise_for_status()
-character_json = character_data.json()
-character = character_json['docs'][0]["name"]
+    # getting quote movie
+    movie_id = quote_json["movie"]
+    movie_data = requests.get(url=f"{URL}movie/{movie_id}", headers=headers)
+    movie_data.raise_for_status()
+    movie_json = movie_data.json()
+    movie = movie_json['docs'][0]['name']
 
-# getting quote movie
-movie_id = quote_json["movie"]
-movie_data = requests.get(url=f"{URL}movie/{movie_id}", headers=headers)
-movie_data.raise_for_status()
-movie_json = movie_data.json()
-movie = movie_json['docs'][0]['name']
-
-# getting character wiki link
-try:
-    wiki_link = character_json['docs'][0]["wikiUrl"]
-except (ValueError, RuntimeError, TypeError, NameError):
-    wiki_link = None
-
-print(quote)
-print(character_json)
-print(movie)
-print(wiki_link)
+    # getting character wiki link
+    try:
+        wiki_link = character_json['docs'][0]["wikiUrl"]
+    except (ValueError, RuntimeError, TypeError, NameError):
+        wiki_link = None
+    return quote, character, wiki_link, movie
 
 
-@app.route('/')
+initial_quote, initial_character, initial_wiki_link, initial_movie = generate_quote()
+
+@app.route('/', methods=['GET', 'POST'])
 def home():
-    return render_template('index.html', quote=quote, character=character, movie=movie, link=wiki_link)
+    if request.method == 'POST' and request.form.get('generate_quote') == 'true':
+        quote, character, wiki_link, movie = generate_quote()
+        return render_template('index.html', quote=quote, character=character, movie=movie, link=wiki_link)
+
+    return render_template('index.html', quote=initial_quote, character=initial_character, movie=initial_movie, link=initial_wiki_link)
+
+print(initial_character)
 
 if __name__ == "__main__":
     app.run(debug=True, use_reloader=False)
